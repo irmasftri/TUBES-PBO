8efe33cae660a02d45b1a25810939aea407d25e8
irmasftri <50016976+irmasftri@users.noreply.github.com>
2019-05-02 18:21:57 +0700

    db yang dipake yang namanya smiati_update yaa

--- DataBase/smiati_update.sql
+++ DataBase/smiati_update.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 27, 2019 at 09:43 AM
+-- Generation Time: May 02, 2019 at 01:21 PM
 -- Server version: 10.1.9-MariaDB
 -- PHP Version: 7.0.0
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `test`
+-- Database: `smiati`
 --
 
 -- --------------------------------------------------------
@@ -36,6 +36,7 @@ CREATE TABLE `adminakun` (
 --
 
 INSERT INTO `adminakun` (`Username`, `Password`) VALUES
+('admin', 'admin'),
 ('Hiskia', '1234567');
 
 -- --------------------------------------------------------
@@ -49,6 +50,16 @@ CREATE TABLE `akunalumni` (
   `NIM` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `akunalumni`
+--
+
+INSERT INTO `akunalumni` (`Password`, `NIM`) VALUES
+('1999-01-22', '14117067'),
+('1999-01-03', '14117071'),
+('2000-02-21', '14117086'),
+('1999-01-01', '14117091');
+
 -- --------------------------------------------------------
 
 --
@@ -58,7 +69,7 @@ CREATE TABLE `akunalumni` (
 CREATE TABLE `alumni` (
   `NIM` varchar(10) NOT NULL,
   `NAMA` varchar(25) NOT NULL,
-  `Tahun_Lulu` char(4) NOT NULL,
+  `Tahun_Lulus` char(4) NOT NULL,
   `Tempat_KP` varchar(30) NOT NULL,
   `IPK` double NOT NULL,
   `Riwayat_Kerja` varchar(30) NOT NULL,
@@ -70,10 +81,31 @@ CREATE TABLE `alumni` (
 -- Dumping data for table `alumni`
 --
 
-INSERT INTO `alumni` (`NIM`, `NAMA`, `Tahun_Lulu`, `Tempat_KP`, `IPK`, `Riwayat_Kerja`, `Judul_TA`, `email`) VALUES
+INSERT INTO `alumni` (`NIM`, `NAMA`, `Tahun_Lulus`, `Tempat_KP`, `IPK`, `Riwayat_Kerja`, `Judul_TA`, `email`) VALUES
 ('14117067', 'Irma Safitri', '2019', '', 4, '', '', 'irma.14117067@student.itera.ac.id'),
+('14117071', 'nurul fauziah azizah', '2021', '-', 3.4, 'oss', '-', '-'),
 ('14117086', 'Nurtias Rahayu', '2019', '', 3, '', '', 'nurtias.14117086@student.itera.ac.id'),
-('14117091', 'Hiskia Perdamen Pulungan', '2021', '', 3, '', '', 'hiskia.14117091@student.itera.ac.id');
+('14117091', 'Hiskia Perdamen Pulungan', '2021', 'jakarta', 3, '', '', 'hiskia.14117091@student.itera.ac.id');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lowongan_kerja`
+--
+
+CREATE TABLE `lowongan_kerja` (
+  `nama_perusahaan` varchar(30) NOT NULL,
+  `jabatan` varchar(25) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `alamat` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lowongan_kerja`
+--
+
+INSERT INTO `lowongan_kerja` (`nama_perusahaan`, `jabatan`, `email`, `alamat`) VALUES
+('Buka Lapak', 'Programmer', 'customerservice@buka-lapak.com', 'Jakarta');
 
 --
 -- Indexes for dumped tables
@@ -89,6 +121,7 @@ ALTER TABLE `adminakun`
 -- Indexes for table `akunalumni`
 --
 ALTER TABLE `akunalumni`
+  ADD PRIMARY KEY (`NIM`),
   ADD KEY `NIM` (`NIM`);
 
 --
@@ -96,6 +129,12 @@ ALTER TABLE `akunalumni`
 --
 ALTER TABLE `alumni`
   ADD PRIMARY KEY (`NIM`);
+
+--
+-- Indexes for table `lowongan_kerja`
+--
+ALTER TABLE `lowongan_kerja`
+  ADD PRIMARY KEY (`email`);
 
 --
 -- Constraints for dumped tables
